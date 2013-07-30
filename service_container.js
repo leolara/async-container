dea1e794b8be02c016cbf5e343fc6cddb9e22f93
Leo Lara <leo@leolara.me>
2013-07-30 21:33:10 +0100

    Fix many small issues

--- service_container.js
+++ service_container.js
@@ -18,28 +18,6 @@ ServiceContainer.prototype.addDefinition = function (id, def) {
     this.service_definitions[id] = def;
 }
 
-ServiceContainer.prototype.getArray = function (ids, callback) {
-    var count = ids.length;
-    var self = this;
-
-    function sendAll() {
-        var args = [null];
-        ids.forEach(function (id) {
-            args.push(self.services[id]);
-        });
-        callback.apply(args);
-    }
-
-    ids.forEach(function (id) {
-        self.get(id, function () {
-            count--;
-            if (count == 0) {
-                sendAll();
-            }
-        });
-    });
-}
-
 ServiceContainer.prototype.get = function (id, callback) {
     if (id instanceof Array) {
         return this.getArray(id, callback);
@@ -52,23 +30,36 @@ ServiceContainer.prototype.get = function (id, callback) {
         return;
     }
 
-    var self = this;
     if (!(this.service_definitions[id].depends instanceof Array)) {
         this.callFactory(id);
     } else {
-        this.get(this.service_definitions[id].depends, function () {
-            var args = Array.prototype.slice.call(arguments);
-            args.splice(0,1);
-            args.unshift(
-                self,
-                function (err, service) {
-                    self.factoryCallback(self, id, service);
-                }
-            )
-            this.service_definitions[id].factory.apply(args);
-        });
+        this.callFactoryDepends(id);
     }
 }
+
+ServiceContainer.prototype.getArray = function (ids, callback) {
+    var count = ids.length;
+    var self = this;
+
+    function sendAll() {
+        var args = [null];
+        ids.forEach(function (id) {
+            args.push(self.services[id]);
+        });
+        callback.apply(undefined, args);
+    }
+
+    ids.forEach(function (id) {
+        self.get(id, function () {
+            count--;
+            if (count == 0) {
+                sendAll();
+            }
+        });
+    });
+}
+
+Service
 
 ServiceContainer.prototype.callFactory = function (id) {
     var self = this;
@@ -92,7 +83,7 @@ ServiceContainer.prototype.factoryDependsCreateCallback = function (id) {
                 self.factoryCallback(self, id, service);
             }
         );
-        this.service_definitions[id].factory.apply(args);
+        self.service_definitions[id].factory.apply(undefined, args);
     }
 }
 
@@ -100,7 +91,6 @@ ServiceContainer.prototype.registerWaiting = function (id, callback) {
     if (!(this.waiting[id] instanceof Array)) {
         this.waiting[id] = [];
     }
-    console.log(this.waiting[id]);
 
     this.waiting[id].push(callback);
 
